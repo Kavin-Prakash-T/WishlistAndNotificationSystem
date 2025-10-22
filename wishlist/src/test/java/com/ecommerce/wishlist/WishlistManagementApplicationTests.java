132fcc1b33ae06c9d121618a0521d569e20fa1c6
Kavin-Prakash-T2024cse <tkavinprakash@gmail.com>
2025-10-22 21:30:39 +0530

    chore: Initial project setup using spring initializer - Added dependencies like spring web,thymeleaf,lombok,mysql driver and spring jpa

---
+++ wishlist/src/test/java/com/ecommerce/wishlist/WishlistManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.wishlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WishlistManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
